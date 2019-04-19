bf49acb7396a415a4400da0dc652e2f76310db69
Stanchion <callumgolds@hotmail.com>
2019-04-19 15:30:49 -0500

    init

---
+++ Assets/TestCallbacks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCallbacks : Bolt.GlobalEventListener
+{
+
+    public override void SceneLoadLocalDone(string scene)
+    {
+        if (BoltNetwork.IsServer)
+        {
+            BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Cube);
+            entity.TakeControl();
+
+        }
+    }
+
+    public override void SceneLoadRemoteDone(BoltConnection connection)
+    {
+        if (BoltNetwork.IsServer)
+        {
+            BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Cube);
+            entity.AssignControl(connection);
+
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/TestController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+
+public class TestController : Bolt.EntityEventListener<ITestState>
+{
+
+
+    public CharacterController _cc;
+    public override void Attached()
+    {
+        _cc = GetComponent<CharacterController>();
+        state.SetTransforms(state.Transform, transform);
+    }
+
+
+
+    public override void SimulateController()
+    {
+        ITestCommandInput input = TestCommand.Create();
+        input.Forward = Input.GetKey(KeyCode.W);
+        input.Back = Input.GetKey(KeyCode.S);
+        input.Left = Input.GetKey(KeyCode.A);
+        input.Right = Input.GetKey(KeyCode.D);
+
+        entity.QueueInput(input);
+    }
+
+    public override void ExecuteCommand(Command command, bool resetState)
+    {
+
+        TestCommand cmd = (TestCommand)command;
+
+        if (resetState)
+        {
+            //owner has sent a correction to the controller
+            transform.position = cmd.Result.Position;
+
+        }
+        else
+        {
+            if (cmd.Input.Forward)
+                _cc.Move(Vector3.forward * Time.fixedDeltaTime * 10f);
+            else if (cmd.Input.Back)
+                _cc.Move(Vector3.back * Time.fixedDeltaTime * 10f);
+            if (cmd.Input.Left)
+                _cc.Move(Vector3.left * Time.fixedDeltaTime * 10f);
+            else if (cmd.Input.Right)
+                _cc.Move(Vector3.right * Time.fixedDeltaTime * 10f);
+
+            cmd.Result.Position = transform.position;
+
+        }
+    }
+
+}
